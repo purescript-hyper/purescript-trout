41409fa505405b197aa3f75efab679b045154e13
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-03-05 14:03:05 +0100

    Initial commit, extracted from main Hyper repo

---
+++ test/Main.purs
@@ -0,0 +1,11 @@
+module Test.Main where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import Node.FS (FS)
+import Test.Spec.Discovery (discover)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (RunnerEffects, run)
+
+main :: Eff (RunnerEffects (fs :: FS)) Unit
+main = discover "Hyper\\.Routing\\..*Spec" >>= run [consoleReporter]
